7bc91fcd73e0fb9d36c1d610cc73fbb557163e0c
Memiş Ali Tufan <67204706+Tufan17@users.noreply.github.com>
2023-12-09 22:48:45 +0300

    Update index.tsx

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -4,8 +4,37 @@ import { quaternaryColor } from "../../constants/color";
 import { Card } from "@mui/material";
 import { FcAssistant, FcManager, FcPhoneAndroid, FcSettings } from "react-icons/fc";
 const UsersView = () => {
-        </> );
+    const user = JSON.parse(window.localStorage.getItem("user")!);
+    return (<div style={{
         padding: '10px',
+    }}>
+        <Group>
+            <Avatar size={64} src={user.avatar} />
+            <Container
+                style={{
+                    marginLeft: '0px',
+                    padding: '0px',
+                }}
+            >
+                <Title order={3} >
+                    Merhaba, Hoş Geldiniz 👋
+                </Title>
+                <Text size="20px">
+                    {user?.nickname}
+                </Text>
+            </Container>
+            <Container style={{
+                width: '100%',
+                height: window.innerWidth / 2,
+                backgroundColor: quaternaryColor,
+                borderRadius: '10px',
+            }}>
+                Banner
+            </Container>
+            
+            <Grid gutter="sm" style={{
+                width: '100%',
+                height: window.innerWidth / 2,
             }} >
                 <Grid.Col span={3}> <Card
                     style={{
